9c647a769a6994f4376f81fb57974de87f4e13bf
Mostafa <leilabadi@gmail.com>
2023-12-21 03:31:40 +0000

    Created more complex scenarios

--- AsyncPractices/Program.cs
+++ AsyncPractices/Program.cs
@@ -6,15 +6,62 @@ public class Program
 
     public static async Task Main(string[] args)
     {
-        var result = await GetContentAsync();
-        Console.WriteLine(result);
+        PrintMessage("Beginning of main method");
+
+        Console.WriteLine();
+
+        Task task = IoBoundOperationAsync();
+        PrintMessage("After getting task for async method");
+        await task;
+        PrintMessage("After awaiting async method completion");
+
+        Console.WriteLine();
+
+        task = Task.Run(async () =>
+        {
+            PrintMessage("Beginning of lambda Task.Run");
+            await IoBoundOperationAsync();
+            PrintMessage("End of lambda Task.Run");
+        });
+        PrintMessage("After getting task for lambda Task.Run ");
+        await task;
+        PrintMessage("After awaiting lambda Task.Run completion");
+
+        Console.WriteLine();
+
+        task = Task.Run(CpuBoundOperation);
+        PrintMessage("After getting task for inline Task.Run");
+        await task;
+        PrintMessage("After awaiting inline Task.Run");
+
+        Console.WriteLine();
+
+        PrintMessage("End of main method");
     }
 
-    private static async Task<string> GetContentAsync()
+    private static async Task IoBoundOperationAsync()
     {
+        PrintMessage("Beginning of async IO operation");
         var client = new HttpClient();
         var response = await client.GetAsync(Url);
-        var content = await response.Content.ReadAsStringAsync();
-        return content;
+        await response.Content.ReadAsStringAsync();
+        PrintMessage("End of async IO operation");
+    }
+
+    static void CpuBoundOperation()
+    {
+        PrintMessage("Beginning of async CPU operation");
+        for (int i = 0; i < 5; i++)
+        {
+            PrintMessage($"Work Item {i}");
+            // Simulate CPU-bound work
+            for (int j = 0; j < 100000000; j++) { }
+        }
+        PrintMessage("End of async CPU operation");
+    }
+
+    private static void PrintMessage(string message)
+    {
+        Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} - {message}");
     }
 }
