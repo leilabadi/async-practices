f9490d1b48283f26f96a4d0c0ca8110208bda189
Mostafa <leilabadi@gmail.com>
2023-12-21 02:02:33 +0000

    Added a simple async logic

---
+++ AsyncPractices/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Async_Practices;
+
+public class Program
+{
+    private static readonly string Url = "https://www.google.com";
+
+    public static async Task Main(string[] args)
+    {
+        var result = await GetContentAsync();
+        Console.WriteLine(result);
+    }
+
+    private static async Task<string> GetContentAsync()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync(Url);
+        var content = await response.Content.ReadAsStringAsync();
+        return content;
+    }
+}
